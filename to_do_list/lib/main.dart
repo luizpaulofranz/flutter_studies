b6e829e8a1d7594a768d6537d9d3adf488f3fe13
luizpaulofranz <luizpaulofranz@gmail.com>
2020-05-07 23:53:39 -0300

    Adds path dependency and funcs to save/read data.

--- to_do_list/lib/main.dart
+++ to_do_list/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(
@@ -14,6 +18,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _todoList = [];
+
+  Future<File> _getJsonFile() async {
+    // to get an hidden app folder to save our data, for Ios or Android
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/appData.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_todoList);
+    final file = await _getJsonFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _getData() async {
+    try {
+      final file = await _getJsonFile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
