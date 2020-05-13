5880829726c57a6d4d20769926429489162c08a2
luizpaulofranz <luizpaulofranz@gmail.com>
2020-05-13 20:41:56 -0300

    Adds app GUI.

--- gif_search/lib/main.dart
+++ gif_search/lib/main.dart
@@ -4,5 +4,16 @@ import 'package:gif_search/ui/home_page.dart';
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(
+      hintColor: Colors.white,
+      primaryColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+        enabledBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+        focusedBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: Colors.blue)),
+        hintStyle: TextStyle(color: Colors.amber),
+      ),
+    ),
   ));
 }
--- gif_search/lib/ui/home_page.dart
+++ gif_search/lib/ui/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,19 +10,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  final String trendingGifsUrl = "https://api.giphy.com/v1/gifs/trending?api_key=qDpZI23fCCocn0Co831jNndZkCkQNYih&limit=20&rating=G";
-  final String searchGifs = "https://api.giphy.com/v1/gifs/search?api_key=qDpZI23fCCocn0Co831jNndZkCkQNYih&q=dogs&limit=20&offset=0&rating=G&lang=en";
+  final String trendingGifsUrl =
+      "https://api.giphy.com/v1/gifs/trending?api_key=qDpZI23fCCocn0Co831jNndZkCkQNYih&limit=20&rating=G";
+  final String searchGifs =
+      "https://api.giphy.com/v1/gifs/search?api_key=qDpZI23fCCocn0Co831jNndZkCkQNYih&q=dogs&limit=20&offset=0&rating=G&lang=en";
 
   String _search;
   int _offset = 0;
 
   Future<Map> _getGifs() async {
     http.Response response;
-    if(_search==null)
+    if (_search == null)
       response = await http.get(trendingGifsUrl);
     else
-      response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=qDpZI23fCCocn0Co831jNndZkCkQNYih&q=$_search&limit=20&offset=$_offset&rating=G&lang=en");
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=qDpZI23fCCocn0Co831jNndZkCkQNYih&q=$_search&limit=20&offset=$_offset&rating=G&lang=en");
 
     return json.decode(response.body);
   }
@@ -30,13 +33,44 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    _getGifs().then((data) => print(data));
+    //_getGifs().then((data) => print(data));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: Image.network(
+            "https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintStyle: TextStyle(color: Colors.white),
+                labelText: "Pesquise aqui...",
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                ),
+                border: OutlineInputBorder(),
+              ),
+              style: TextStyle(color: Colors.white, fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _getGifs(),
+              builder: null,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
