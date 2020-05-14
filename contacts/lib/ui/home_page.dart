62d26152f31d823b76a21b646143fab7aca30f80
luizpaulofranz <luizpaulofranz@gmail.com>
2020-05-14 14:51:29 -0300

    Adds ListView for contacts list.

--- contacts/lib/ui/home_page.dart
+++ contacts/lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:contacts/model/contact.dart';
 import 'package:contacts/repository/contact_repository.dart';
 import 'package:flutter/material.dart';
@@ -10,8 +12,87 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   ContactRepository repository = ContactRepository();
 
+  List<Contact> contacts = List();
+
+  @override
+  void initState() {
+    super.initState();
+    repository.getAll().then((value) {
+      print(value);
+      setState(() {
+        contacts = value;
+      });
+    });
+  }
+
+  Widget _contactCard(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: contacts[index].imgPath != null
+                        ? DecorationImage(
+                            image: FileImage(File(contacts[index].imgPath)))
+                        : DecorationImage(
+                            image: AssetImage("assets/person.png"))),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      contacts[index].name ?? "",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      contacts[index].email ?? "",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      contacts[index].phone ?? "",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // as we saw before, Scaffold is an Material implementation that allows us to usa other material resources, like a floating button
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+          return _contactCard(context, index);
+        },
+      ),
+    );
   }
 }
