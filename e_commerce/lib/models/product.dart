3846fcf98608256793eff1946d536996a1c95ded
luizpaulofranz <luizpaulofranz@gmail.com>
2020-08-18 22:44:00 -0300

    Adds model for products.

---
+++ e_commerce/lib/models/product.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Product {
+
+  String category;
+  String id;
+
+  String title;
+  String description;
+
+  double price;
+
+  List images;
+  List sizes;
+
+  Product.fromDocument(DocumentSnapshot document) {
+    id = document.documentID;
+    title = document.data["title"];
+    description = document.data["description"];
+    price = document.data["price"] + 0.0;
+    images = document.data["images"];
+    sizes = document.data["sizes"];
+  }
+
+}
