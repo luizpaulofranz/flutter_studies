2fd220ab7ef4521f49a4efa46cececa55c74bf90
luizpaulofranz <luizpaulofranz@gmail.com>
2020-08-31 00:19:24 -0300

    Adds product page carousel.

--- e_commerce/lib/screens/product_screen.dart
+++ e_commerce/lib/screens/product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:e_commerce/models/product.dart';
 import 'package:flutter/material.dart';
 
@@ -19,8 +20,54 @@ class _ProductScreenState extends State<ProductScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Product Page"),
+
+    final Color primaryColor = Theme.of(context).primaryColor;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: Carousel(
+              images: product.images.map((url) => NetworkImage(url)).toList(),
+              dotSize: 4,
+              dotSpacing: 15,
+              dotBgColor: Colors.transparent,
+              dotColor: primaryColor,
+              autoplay: false,
+              //autoplayDuration: Duration(milliseconds: 3000),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  product.title,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500
+                  ),
+                  maxLines: 3,
+                ),
+                Text(
+                  "R\$ ${product.price}",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: primaryColor
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
