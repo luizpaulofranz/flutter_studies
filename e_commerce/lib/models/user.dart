c43508e671e4839be0500a1f267713665c5696f8
luizpaulofranz <luizpaulofranz@gmail.com>
2020-09-02 23:35:44 -0300

    Adds actions to create user on Firebase - SignUp.

--- e_commerce/lib/models/user.dart
+++ e_commerce/lib/models/user.dart
@@ -1,11 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class User extends Model {
 
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseUser firebaseUser;
+  Map<String, dynamic> userData = Map();
+
+
   bool isLoading = false;
 
-  void signUp() {
+  Future _saveUserData(Map<String, dynamic> userData) async {
+    this.userData = userData;
+    // with this, we save data on firebase database, this is because FirebaseAuth only saves the user e-mail, and with this we sava aditional data.
+    await Firestore.instance.collection("users").document(firebaseUser.uid).setData(userData);
+  }
 
+  void signUp({@required Map<String, dynamic> userData, @required String pass, @required VoidCallback onSuccess, @required VoidCallback onFail}) {
+    // to get loading state there on pageð
+    isLoading = true;
+    notifyListeners();
+
+    _auth.createUserWithEmailAndPassword(
+      email: userData["email"], 
+      password: pass
+    ).then((user) async {
+      firebaseUser = user.user;
+
+      await _saveUserData(userData);
+
+      onSuccess();
+      isLoading = false;
+      notifyListeners();
+    }).catchError((e) {
+
+      onFail();
+      isLoading = false;
+      notifyListeners();
+    });
   }
 
   void signIn() async {
