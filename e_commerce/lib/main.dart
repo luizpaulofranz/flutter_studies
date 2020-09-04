42c24c739ae9fd9b169f6afb3f8851fdcb422c70
luizpaulofranz <luizpaulofranz@gmail.com>
2020-09-03 23:56:14 -0300

    Adds combination of ScopedModels one feeding other

--- e_commerce/lib/main.dart
+++ e_commerce/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/models/cart.dart';
 import 'package:e_commerce/models/user.dart';
 import 'package:e_commerce/screens/home_screen.dart';
 import 'package:flutter/material.dart';
@@ -15,14 +16,23 @@ class MyApp extends StatelessWidget {
     // look at User model de function notifyListeners().
     return ScopedModel<User>(
       model: User(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter\'s Clothing',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          primaryColor: Color.fromARGB(255, 4, 125, 141)
-        ),
-        home: HomeScreen(),
+      // we can combine ScopedModels and Descendants.
+      // in this case, we want to pass the current user immediatly to our Cart model
+      child: ScopedModelDescendant<User>(
+        builder: (context, child, model) {
+          return ScopedModel<Cart>(
+            model: Cart(model),
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Flutter\'s Clothing',
+              theme: ThemeData(
+                primarySwatch: Colors.blue,
+                primaryColor: Color.fromARGB(255, 4, 125, 141)
+              ),
+              home: HomeScreen(),
+            ),
+          );
+        },
       ),
     );
   }
