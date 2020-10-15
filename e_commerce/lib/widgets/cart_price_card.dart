5758c082f84e384dddd216efe83769a11cc59246
luizpaulofranz <luizpaulofranz@gmail.com>
2020-10-15 00:01:49 -0300

    Calls the methods to calculate the price on cart.

--- e_commerce/lib/widgets/cart_price_card.dart
+++ e_commerce/lib/widgets/cart_price_card.dart
@@ -16,6 +16,11 @@ class CartPriceCard extends StatelessWidget {
         padding: EdgeInsets.all(16),
         child: ScopedModelDescendant<Cart>(
           builder: (context, child, model) {
+            
+            double price = model.getProductsPrice();
+            double discount = model.getDiscount();
+            double ship = model.getShipPrice();
+
             return Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
@@ -28,7 +33,7 @@ class CartPriceCard extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text("Subtotal"),
-                    Text("R\$ 0.00"),
+                    Text("R\$ ${price.toStringAsFixed(2)}"),
                   ],
                 ),
                 Divider(),
@@ -36,7 +41,7 @@ class CartPriceCard extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text("Desconto"),
-                    Text("R\$ 0.00"),
+                    Text("R\$ ${discount.toStringAsFixed(2)}"),
                   ],
                 ),
                 Divider(),
@@ -44,7 +49,7 @@ class CartPriceCard extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text("Entrega"),
-                    Text("R\$ 0.00"),
+                    Text("R\$ ${ship.toStringAsFixed(2)}"),
                   ],
                 ),
                 Divider(),
@@ -53,7 +58,7 @@ class CartPriceCard extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text("Total", style: TextStyle(fontWeight: FontWeight.w500)),
-                    Text("R\$ 0.00", style: TextStyle(color: Theme.of(context).primaryColor, fontSize: 16)),
+                    Text("R\$ ${(price+ship-discount).toStringAsFixed(2)}", style: TextStyle(color: Theme.of(context).primaryColor, fontSize: 16)),
                   ],
                 ),
                 SizedBox(height: 12),
